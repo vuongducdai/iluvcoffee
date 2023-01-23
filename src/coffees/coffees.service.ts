2c1534798d662874cee10cb7b9be11995c87d015
Vuong Duc Dai <>
2023-01-23 22:26:07 +0700

    Setup TypeORM with Repository

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,25 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto.ts/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/create-coffee.dto.ts/update-coffee.dto';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
+
   findAll(limit: string, offset: string) {
-    return `This returns all coffees ${limit}, ${offset}`;
+    return this.coffeeRepository.find();
   }
 
-  findOne(id: string) {
-    return `This return coffees id ${id}`;
+  async findOne(id: string) {
+    const coffee = await this.coffeeRepository.findOneBy({ id: +id });
+    if (coffee) {
+      throw new NotFoundException(`Not found #${id} coffee`);
+    }
+    return coffee;
   }
 
-  create(body: CreateCoffeeDto) {
-    return body;
+  create(createCoffeeDto: CreateCoffeeDto) {
+    const newCoffee = this.coffeeRepository.create(createCoffeeDto);
+    return this.coffeeRepository.save(newCoffee);
   }
 
-  update(id: string, body: CreateCoffeeDto) {
-    return `This return coffees id ${id}`;
+  async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const coffee = await this.coffeeRepository.preload({
+      id: +id,
+      ...updateCoffeeDto,
+    });
+
+    if (!coffee) {
+      throw new NotFoundException(`Not found #${id} coffee`);
+    }
+
+    return this.coffeeRepository.save(coffee);
   }
 
-  remove(id: string) {
-    return 'this delete';
+  async remove(id: string) {
+    const coffee = await this.findOne(id);
+    return this.coffeeRepository.remove(coffee);
   }
 }
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   brand: string;
+
+  @Column()
   name: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
